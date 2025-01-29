4ce3202097609bf9250d02b81f526d28814f5d29
Dmitriy RonMan <dmitriykolesnikov7@gmail.com>
2025-01-29 19:48:40 +0500

    add new test

--- tests/template.spec.js
+++ tests/template.spec.js
@@ -1,14 +1,17 @@
-import { test, expect } from '@playwright/test';
+import {test, expect} from '@playwright/test';
 import {faker} from "@faker-js/faker";
 import {MainPage} from "../src/pages/mainPages";
 import {RegisterPage} from "../src/pages/registerPage";
 import {YourfeedPage} from "../src/pages/yourfeedPage";
 
 const URL = 'https://realworld.qa.guru/';
+const Text = (number) => {
+    return faker.lorem.words(number);
+};
 
 test.describe('Шаблон', () => {
 
-    test.beforeEach(async ({ page }) => {
+    test.beforeEach(async ({page}) => {
         const mainPage = new MainPage(page);
         const registerPage = new RegisterPage(page);
         const yourfeedPage = new YourfeedPage(page);
@@ -28,6 +31,44 @@ test.describe('Шаблон', () => {
         const yourfeedPage = new YourfeedPage(page);
         await yourfeedPage.goToArticle();
         await expect(yourfeedPage.profileNameField).toBeVisible();
-
+        await page.getByPlaceholder('Article Title').click();
+        await page.getByPlaceholder('Article Title').fill(Text(5));
+        await page.getByPlaceholder('What\'s this article about?').click();
+        await page.getByPlaceholder('What\'s this article about?').fill(Text(10));
+        await page.getByPlaceholder('Write your article (in markdown)').click();
+        await page.getByPlaceholder('Write your article (in markdown)').fill(Text(15));
+        await page.getByPlaceholder('Enter tags').click();
+        await page.getByPlaceholder('Enter tags').fill(Text(3));
+        await page.getByRole('button', {name: 'Publish Article'}).click();
+        await expect(page.getByRole('button', {name: 'Post Comment'})).toBeVisible();
     });
+
+    test('Пользователь может оставить комментарий к своей статье', async ({page,}) => {
+        const yourfeedPage = new YourfeedPage(page);
+        await yourfeedPage.goToArticle();
+        await expect(yourfeedPage.profileNameField).toBeVisible();
+        await page.getByPlaceholder('Article Title').click();
+        await page.getByPlaceholder('Article Title').fill(Text(5));
+        await page.getByPlaceholder('What\'s this article about?').click();
+        await page.getByPlaceholder('What\'s this article about?').fill(Text(10));
+        await page.getByPlaceholder('Write your article (in markdown)').click();
+        await page.getByPlaceholder('Write your article (in markdown)').fill(Text(15));
+        await page.getByPlaceholder('Enter tags').click();
+        await page.getByPlaceholder('Enter tags').fill(Text(3));
+        await page.getByRole('button', {name: 'Publish Article'}).click();
+        await expect(page.getByRole('button', {name: 'Post Comment'})).toBeVisible();
+        await page.getByPlaceholder('Write a comment...').click();
+        await page.getByPlaceholder('Write a comment...').fill("Отличная статья!");
+        await page.getByRole('button', {name: 'Post Comment'}).click();
+        await expect(page.locator('.card-text')).toContainText("Отличная статья!");
+    });
+
+    test('Пользователь может изменить пароль и залогиниться с новым паролем)', async ({page,}) => {
+       const yourfeedPage = new YourfeedPage(page);
+       const registerPage = new RegisterPage(page);
+       const newPassword = faker.internet.password();
+       await expect(registerPage.usernameField).toContainText();
+
+   });
 });
+
